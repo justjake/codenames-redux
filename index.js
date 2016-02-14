df2189d428a39e74088c4ebf6a1c190fb741e547
Jake Teton-Landis <jake.tl@airbnb.com>
2016-02-14 01:24:14 -0800

    also show how highlighting the kill word looks

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import Board from './models/Board';
+import { KILL } from './constants';
 
 function main() {
   const board = new Board();
   board.pick(board.wordsInBoard[0]);
   board.pick(board.wordsInBoard[1]);
+  const kill = board.wordsInBoard.filter(w => board.teamOf(w) === KILL)[0]
+  board.pick(kill);
   console.log(board.inspect());
+  console.log(board.getDepleted());
 }
 
 main();
